17fb0f736102defa3e81842b02075a7044b0626d
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-02-09 17:28:00 +0700

    Добавьте файлы проекта.

---
+++ Programm/Programm.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+//laba 1
+using namespace std;
+
+double Stepen(double x, int n) {
+	double step = x;
+	for (int i = 1; i < n; i++) {
+		step *= x;
+	}
+	return step;
+};
+
+long double Factorial(long double y) {
+	if (y < 0)
+		return 0;
+	if (y == 0)
+		return 1;
+	else 
+		return y * Factorial(y - 1);
+};
+
+double Cosinus(double x) {
+	double cos = 1, eps = 0.0001;
+	int n = 1;
+	while ((Stepen(x, n * 2) / Factorial(n * 2)) >= eps) {
+		cos = cos + (Stepen(-1, n) * Stepen(x, n*2) / Factorial(n*2));
+		n++; 
+	}
+	
+	return cos;
+};
+
+int main() {
+	cout << Cosinus(4) << endl;
+}
---
+++ lab2/lab2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+void Polozhitelnie() {
+    int n;
+    cin >> n;
+    while (n != 0) {
+        if (n > 0)
+            cout << n << " ";
+        Polozhitelnie();
+    }
+    cout << endl;
+};
+
+void Chisla(int A[], int n) {
+    
+
+};
+
+
+int main() {
+    int n = 10;
+    int A[] = { 1,-2,3,-4,5, -6,7,-8,9,-10 };
+    Chisla(A, n);
+}
