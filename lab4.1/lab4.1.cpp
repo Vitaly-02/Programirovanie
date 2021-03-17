ae0ec201e8550df2e0a032be2901c63391399d3a
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-03-17 12:00:48 +0700

    4 lab

---
+++ lab4.1/lab4.1.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int random(int N) {
+    return rand() % N;
+}
+
+int main() {
+ /*   int n = 5, m, **B, **C;
+    cin >> m;
+    B = new int *[m];
+    for (int i = 0; i < m; i++) {
+        B[i] = new int[n];
+    }
+    if (B == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+    C = new int* [m-1];
+    for (int i = 0; i < m-1; i++) {
+        C[i] = new int[n-1];
+    }
+    if (C == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            B[i][j] = rand() % 10;
+        }
+    }
+  */
+    srand(time(NULL));
+    int n = 5, m;
+    cin >> m;
+    int** B;
+    B = new int* [n];
+    for (int i = 0; i < n; i++) {
+        B[i] = new int[m];
+    }
+    if (B == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+    int** C;
+    C = new int* [n-1];
+    for (int i = 0; i < n; i++) {
+        C[i] = new int[m-1];
+    }
+    if (C == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            B[i][j] = random(11);
+            cout << B[i][j] << " ";
+            if (j == m - 1) {
+                cout << endl;
+            }
+        }
+    }
+    int a = 0, b = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (B[a][b] < B[i][j]) {
+                a = i;
+                b = j;
+            }
+        }
+    }
+    cout << endl;
+    cout << a << " " << b;
+    cout << endl;
+    for (int i = 0, k = 0; i < n, k < n - 1; i++, k++) {
+        for (int j = 0, l = 0; j < m, l < m - 1; j++, l++) {
+            int y = 0;
+            if (i == a) {
+                y = 1;
+            }
+            if (j == b) {
+                y = 2;
+            }
+            if ((i == a)&&(j == b)) {
+                y = 3;
+            }
+            switch (y) {
+            case 0:
+                break;
+            case 1:
+                i++;
+                break;
+            case 2:
+                j++;
+                break;
+            case 3:
+                i++;
+                j++;
+                break;
+            }
+            C[k][l] = B[i][j];
+            cout << C[k][l] << " ";
+            if (j == m - 2) {
+                cout << endl;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        B[i] = NULL;
+    }
+    B = NULL;
+    for (int i = 0; i < n-1; i++) {
+        C[i] = NULL;
+    }
+    C = NULL;
+}
---
+++ lab4.2/lab4.2.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int random(int N) {
+    return rand() % N;
+}
+
+int main() {
+    srand(time(NULL));
+    int n, m;
+    cin >> n >> m;
+    int** B;
+    B = new int* [n];
+    for (int i = 0; i < n; i++) {
+        B[i] = new int[m];
+    }
+    if (B == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+    int** C;
+    C = new int* [n + 1];
+    for (int i = 0; i < n + 1; i++) {
+        C[i] = new int[m + 1];
+    }
+    if (C == NULL) {
+        printf("Error pizdec");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            B[i][j] = random(11);
+        }
+    }
+    
+    int sum = 0, sumi = 0, sumj = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+                C[i][j] = B[i][j];
+                sumi += B[i][j];
+                sum += B[i][j];
+        }
+        C[i][m] = sumi;
+        sumi = 0;
+    }
+    C[n][m] = sum;
+    int j = 0; int i = 0;
+    while (j < m) {
+        for (i = 0; i < n; i++) {
+            sumj += B[i][j];
+            
+        }
+        C[n][j] = sumj;
+        sumj = 0;
+        j++;
+    }
+
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << B[i][j] << " ";
+            if (j == m - 1) {
+                cout << endl;
+            }
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n+1; i++) {
+        for (int j = 0; j < m+1; j++) {
+            cout << C[i][j] << " ";
+            if (j == m) {
+                cout << endl;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        B[i] = NULL;
+    }
+    B = NULL;
+    for (int i = 0; i < n + 1; i++) {
+        C[i] = NULL;
+    }
+    C = NULL;
+}
