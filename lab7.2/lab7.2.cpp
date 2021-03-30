efaef9389f3dc373e1abbc12a0ce94676edf52ac
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-03-31 00:27:13 +0700

    lab7

--- lab7.2/lab7.2.cpp
+++ lab7.2/lab7.2.cpp
@@ -8,7 +8,21 @@ int random(int N) {
     return rand() % N + 1;
 }
 
+void SumStr(int** Mas, int size) {
+    cout << endl;
+    int temp = 0;
+    for (int i = 0; i < size; i++) {
+        temp = 0;
+        for (int j = 0; j < Mas[i][0]; j++) {
+            temp += Mas[i][j];
+        }
+        cout << temp << endl;
+    }
+    cout << endl;
+}
+
 int main() {
+    srand(time(NULL));
     int** B;
     int N = random(10);
     B = new int* [N];
@@ -23,25 +37,24 @@ int main() {
     }
 
     for (int i = 0; i < N; i++) {
-        cout << B[i][0] << endl;
-
+        for (int j = 1; j < B[i][0]; j++) {
+            B[i][j] = random(10);
+        }
     }
-    cout << "\n\n";
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < B[i][0]; j++) {
-
-            B[i][j] = random(11);
             cout << B[i][j] << " ";
-            if (j == B[i][0]) {
+            if (j == B[i][0] - 1) {
                 cout << endl;
             }
         }
     }
+
+    SumStr(B, N);
 
     for (int i = 0; i < N; i++) {
         B[i] = NULL;
     }
     B = NULL;
 }
-
