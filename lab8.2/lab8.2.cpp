773751db8eb539d65adf7dc5e683d563ef4e73aa
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-04-18 14:27:06 +0700

    8 lab zaebis

--- lab8.2/lab8.2.cpp
+++ lab8.2/lab8.2.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-struct Obshaga { int number; int s; char fak[11]; int living; } S;
+struct Obshaga { int number; int s; string fak; int living; } S;
 
 int random(int a, int b) {
     return a + rand() % (b - a + 1); //rand() % N + 1;
@@ -14,57 +14,80 @@ int main() {
     srand(time(NULL));
     const int n = 10;
     struct Obshaga Massive[n];
-    char A[11] = "Fakultet A";
-    char B[11] = "Fakultet B";
-    char C[11] = "Fakultet C";
+    string A = "Fakultet A";
+    string B = "Fakultet B";
+    string C = "Fakultet C";
+    cout << "Nomer room     Ploshad     Fakultet         Kol-vo students" << endl;
     for (int i = 0; i < 3; i++) {
-        Massive[i] = { random(1, 100), random(10, 20), "Fakultet A", random(1, 3) };
-        cout << Massive[i].number << " " << Massive[i].s << " " << Massive[i].fak << " " << Massive[i].living << endl;
+        Massive[i] = { random(1, 100), random(10, 20), A, random(1, 3) };
+        cout << Massive[i].number << "             " << Massive[i].s << "             " << Massive[i].fak << "             " << Massive[i].living << endl;
     }
-    for (int i = 4; i < 6; i++) {
-        Massive[i] = { random(1, 100), random(10, 20), "Fakultet B", random(1, 3) };
-        cout << Massive[i].number << " " << Massive[i].s << " " << Massive[i].fak << " " << Massive[i].living << endl;
+    for (int i = 3; i < 6; i++) {
+        Massive[i] = { random(1, 100), random(10, 20), B, random(1, 3) };
+        cout << Massive[i].number << "             " << Massive[i].s << "             " << Massive[i].fak << "             " << Massive[i].living << endl;
     }
     for (int i = 6; i < n; i++) {
-        Massive[i] = { random(1, 100), random(10, 20), "Fakultet C", random(1, 3) };
-        cout << Massive[i].number << " " << Massive[i].s << " " << Massive[i].fak << " " << Massive[i].living << endl;
+        Massive[i] = { random(1, 100), random(10, 20), C, random(1, 3) };
+        cout << Massive[i].number << "             " << Massive[i].s << "             " << Massive[i].fak << "             " << Massive[i].living << endl;
     }
-    cout << "\n\n";
-
-    char** Fakultets;
-    Fakultets = new char* [];
     
-    Fakultets[0] = new char[11];
-    Fakultets[0] = Massive[0].fak;
+    string** Fakultets;
+    Fakultets = new string* [11];
+    
+    Fakultets[0] = new string[11];
+    Fakultets[0] = &Massive[0].fak;
     if (Fakultets == NULL) {
         printf("Error pizdec");
         return 1;
     }
-    char temp[11];
-  //  char temp = *Massive[0].fak;
+
     int amountF = 0;
     
     for (int i = 1; i < n; i++) {
-        bool no = 0;
+        bool no = 1;
         for (int j = 0; j < amountF+1; j++) {
-            if ((Fakultets[j] != Massive[i].fak)&&(no == 0)) {
-                amountF++;
-                Fakultets[amountF] = new char[11];
-                Fakultets[amountF] = Massive[i].fak;
-                no = 1;
-                break;
+            if (*Fakultets[j] != Massive[i].fak) {
+                no = no * 1;
             }
+            else {
+                no = 0;
+            }   
+        }
+        if (no == 1) {
+            amountF+=1;
+            Fakultets[amountF] = new string[11];
+            *Fakultets[amountF] = Massive[i].fak;
+            no = 0;
         }
     }
-    cout << "\n\n";
-    for (int i = 0; i < amountF; i++) {
-        cout << Fakultets[i] << " ";
+    cout << endl;
+    for (int i = 0; i < amountF+1; i++) {
+        cout << *Fakultets[i] << endl;
     }
-    cout << amountF;
+    cout << "Vsego fakultetov: " << amountF+1 << "\n\n";
+    cout << "Nazvanie fakulteta    Kol-vo rooms    Kol-vo students      Medium ploshad/student" << endl;
+    struct Info { string fak; int amount_rooms; int amount_students; double plosh; };
+    struct Info *Information;
+    Information = new Info[amountF + 1];
+    for (int i = 0; i < amountF + 1; i++) {
+        Information[i].fak = *Fakultets[i];
+        Information[i].amount_rooms = 0;
+        Information[i].amount_students = 0;
+        Information[i].plosh = 0;
+        for (int j = 0; j < n; j++) {
+            if (Information[i].fak == Massive[j].fak) {
+                Information[i].amount_rooms += 1;
+                Information[i].amount_students += Massive[j].living;
+                Information[i].plosh += Massive[j].s;
+            }
+        }
+        cout << Information[i].fak << "               " << Information[i].amount_rooms << "                  " << Information[i].amount_students << "               " << Information[i].plosh/Information[i].amount_students << endl;
+    }
 
-    for (int i = 0; i < amountF; i++) {
+    for (int i = 0; i < amountF+1; i++) {
         Fakultets[i] = NULL;
     }
     Fakultets = NULL;
+    Information = NULL;
     return 0;
 }
